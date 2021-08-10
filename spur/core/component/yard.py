cab16af275bac14be129cd3cf9a1133c504d5c64
Willem Klumpenhouwer <willemjjk@gmail.com>
2021-08-10 14:19:37 -0400

    Created and tested a simple yard component (#11).

--- spur/core/component/yard.py
+++ spur/core/component/yard.py
@@ -1,11 +1,1 @@
-from simpy.resources.store import Store
 
-from spur.core import Model
-from spur.core.base import StoreComponent
-
-
-class Yard(StoreComponent):
-    __name__ = "Yard"
-
-    def __init__(self, model: Model, uid, store: Store) -> None:
-        super().__init__(model, uid, store)
