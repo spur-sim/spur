b42d072a40112cfc92f5c47d54925c133f6fdf0f
Willem Klumpenhouwer <willemjjk@gmail.com>
2021-08-10 22:31:42 -0400

    Updates to toy model and project description.

--- toy_model.py
+++ toy_model.py
@@ -2,42 +2,30 @@
 
 from spur.core import Model
 from spur.core.route import Route
-from spur.core.component import TimedTrack, PhysicsTrack, SimpleYard
+from spur.core.component import TimedTrack, SimpleYard, SimpleCrossover
 
 # Instantiate a model
 model = Model()
-components = []
-components.append(model.add_component(SimpleYard, "0", "1", "Y", capacity=10))
-components.append(
-    model.add_component(PhysicsTrack, "1", "2", "A", length=80, track_speed=25)
-)
-components.append(
-    model.add_component(TimedTrack, "2", "3", "A", traversal_time=30, capacity=2)
-)
-components.append(
-    model.add_component(PhysicsTrack, "3", "4", "A", length=450, track_speed=25)
-)
-components.append(
-    model.add_component(PhysicsTrack, "4", "5", "A", length=1050, track_speed=25)
-)
-components.append(
-    model.add_component(PhysicsTrack, "5", "6", "A", length=650, track_speed=25)
-)
-components.append(model.add_component(SimpleYard, "6", "7", "A", capacity=10))
-route = Route()
-for c in components:
-    route.append(c)
 
-route.segments[2].departure = 90
-# Plop a train onto the component
-train = model.add_train("CX1", max_speed=20, route=route)
-train = model.add_train("CX2", max_speed=19, route=route)
-train = model.add_train("CX3", max_speed=19, route=route)
-train = model.add_train("CX4", max_speed=19, route=route)
-train = model.add_train("CX5", max_speed=19, route=route)
-train = model.add_train("CX6", max_speed=19, route=route)
-train = model.add_train("CX7", max_speed=19, route=route)
-train = model.add_train("CX8", max_speed=19, route=route)
+north = Route()
+south = Route()
+c = model.add_component(SimpleYard, "00", "01", "Y", capacity=10)
+north.append(c)
+south.append(c)
+c = model.add_component(TimedTrack, "01", "08", "N", traversal_time=84)
+north.append(c)
+c = model.add_component(TimedTrack, "01", "08", "S", traversal_time=84)
+south.append(c)
+c = model.add_component(SimpleCrossover, "08", "09", "X", traversal_time=5)
+north.append(c)
+south.append(c)
+c = model.add_component(SimpleYard, "09", "24", "Y", capacity=10)
+north.append(c)
+south.append(c)
+
+# Plop some trains
+train = model.add_train("CX1", max_speed=20, route=north)
+train = model.add_train("CX2", max_speed=19, route=south)
 model.start()
 model.run()
 # model.run()
