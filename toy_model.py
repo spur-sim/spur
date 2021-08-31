3b9272f9b21ac9b1f3e9b721473eb5c74b3cf4a0
Willem Klumpenhouwer <willemjjk@gmail.com>
2021-08-31 10:24:25 -0400

    Implemented dwell time model for a simple station platform.

--- toy_model.py
+++ toy_model.py
@@ -2,7 +2,7 @@
 
 from spur.core import Model
 from spur.core.route import Route
-from spur.core.component import TimedTrack, SimpleYard, SimpleCrossover
+from spur.core.component import TimedTrack, SimpleYard, SimpleCrossover, SimpleStation
 from spur.core.jitter import UniformJitter
 
 # Instantiate a model
@@ -17,12 +17,22 @@ c = model.add_component(
     TimedTrack, "01", "08", "N", traversal_time=84, jitter=UniformJitter(-5, 5)
 )
 north.append(c)
-c = model.add_component(TimedTrack, "01", "08", "S", traversal_time=84)
+c = model.add_component(
+    TimedTrack, "01", "08", "S", traversal_time=84, jitter=UniformJitter(-5, 5)
+)
 south.append(c)
 c = model.add_component(SimpleCrossover, "08", "09", "X", traversal_time=5)
 north.append(c)
 south.append(c)
-c = model.add_component(SimpleYard, "09", "24", "Y", capacity=10)
+c = model.add_component(
+    SimpleStation, "09", "24", "N", mean_boarding=10, mean_alighting=10
+)
+north.append(c)
+c = model.add_component(
+    SimpleStation, "09", "24", "S", mean_boarding=10, mean_alighting=10
+)
+south.append(c)
+c = model.add_component(SimpleYard, "24", "38", "Y", capacity=10)
 north.append(c)
 south.append(c)
 
