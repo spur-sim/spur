5561cc66d8972e4438e446cfed14df64b35669c1
Willem Klumpenhouwer <willemjjk@gmail.com>
2021-09-28 09:14:53 -0400

    Setting up data for GO Sub model.

--- go_subdivision.py
+++ go_subdivision.py
@@ -2,9 +2,9 @@ from spur.core import Model
 
 
 m = Model()
-m.components_from_json("data/toy_model/component.json")
-m.routes_from_json("data/toy_model/route.json")
-m.trains_from_json("data/toy_model/train.json")
+m.components_from_json("data/models/go_subdivision/components.json")
+m.routes_from_json("data/models/go_subdivision/routes.json")
+m.trains_from_json("data/models/go_subdivision/trains.json")
 
 m.start()
 m.run()
