054ef46f7801401a9bb41e7ddabefb1d76cf961b
Willem Klumpenhouwer <willemjjk@gmail.com>
2022-10-05 14:28:42 -0400

    Some small updates, plus basic editor work.

--- line4.py
+++ line4.py
@@ -4,9 +4,9 @@ import os
 base_path = "data/models/line4/20220208"
 
 m = Model()
-m.components_from_json(os.path.join(base_path, "components.json"))
-m.routes_from_json(os.path.join(base_path, "routes.json"))
-m.trains_from_json(os.path.join(base_path, "trains.json"))
+m.add_components_from_json_file(os.path.join(base_path, "components.json"))
+m.add_routes_from_json_file(os.path.join(base_path, "routes.json"))
+m.add_trains_from_json_file(os.path.join(base_path, "trains.json"))
 
 m.start()
 m.run(until=36900)
